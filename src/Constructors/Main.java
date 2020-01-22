2b8e4f4f7c7b2121bf2ef9ead5f10faa4360a208
ext.augorenko <a.uharenka@andersenlab.com>
2020-01-22 08:30:45 +0300

    Homework constructor is done

---
+++ src/Constructors/Main.java
@@ -0,0 +1,10 @@
+package Constructors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConstructorClass a = new ConstructorClass();
+        ConstructorClass b = new ConstructorClass("Артём");
+    }
+}
+
