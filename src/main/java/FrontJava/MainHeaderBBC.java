93f2dc5f654ae61e91cd3136c8ef11cd9505c00e
Artem <trycheall@yandex.ru>
2020-02-28 18:36:51 +0300

    add logs and refactor codes

--- src/main/java/FrontJava/MainHeaderBBC.java
+++ src/main/java/FrontJava/MainHeaderBBC.java
@@ -1,45 +1,50 @@
 package FrontJava;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.*;
+import java.util.logging.*;
+
 
 public class MainHeaderBBC {
-    public static WebDriver driver;
-    private static By news = By.linkText("News");
-    private static By search = By.id("orb-search-q");
+    public WebDriver driver;
+    private By news = By.linkText("News");
+    private By search = By.id("orb-search-q");
+    private Logger logger = Logger.getLogger("FrontJava.nose");
 
+    public String url() {
+        return driver.getCurrentUrl();
+    }
 
-public static String url(){
-    return driver.getCurrentUrl();
-}
-    public static WebDriver MainBBC(WebDriver driver1) {
-
-         driver1.get("https://www.bbc.com/");
-        driver=driver1;
+    public WebDriver MainBBC(WebDriver driver1) {
+        driver1.get("https://www.bbc.com/");
+        driver = driver1;
         return driver;
-
-
     }
 
-public static void setInputSearch(String text){
-WebElement input = driver.findElement(search);
-    if(input.isDisplayed() && input.isEnabled())
-        input.sendKeys(text);
-}
-    public static String getInputSearch(){
+    public void setInputSearch(String text) {
+        logger.log(Level.INFO, "Вводим в поиск слово текст");
         WebElement input = driver.findElement(search);
-     return input.getAttribute("value");
+        if (input.isDisplayed() && input.isEnabled())
+            input.sendKeys(text);
     }
-public static void menuClick(String itemMenu){
-        switch(itemMenu){
+
+    @Step("ads")
+    public String getInputSearch() {
+        WebElement input = driver.findElement(search);
+        return input.getAttribute("value");
+    }
+
+    public void menuClick(String itemMenu) {
+        logger.log(Level.INFO, "Нажимаем на пункт меню - новости");
+        switch (itemMenu) {
             case ("News"):
                 WebElement menuNews = driver.findElement(news);
                 menuNews.click();
                 break;
-
             default:
                 System.out.println("Not item menu!!");
                 break;
         }
+    }
 
-}
 }
